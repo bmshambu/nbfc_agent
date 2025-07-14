6eab1d94b4e1adb09b148a5c4555b13f2ac087f9
Shambu <46180328+bmshambu@users.noreply.github.com>
2025-07-14 23:05:37 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,11 +1,5 @@
 class AppConfig:
     def __init__(self):
-        # self.api_key = '6cTMjxmlENtbN4zNJcbXr6KrPc5U664wC07oNbtw93ddY3K16LZxJQQJ99BCACHYHv6XJ3w3AAAAACOGcJgk'
-        # self.base_url = 'https://a3316-m85kuo5i-eastus2.openai.azure.com/'
-        # self.version = '2024-12-01-preview'
-        # self.model_name = 'o3-mini'
-        # self.api_key_groq="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"
-        # self.groq_model_name="llama-3.3-70b-versatile"
         self.api_key = "AIzaSyC5XasXZlcarmvDHodbV5R2TPPURfzxANU"
         self.base_url = ''
         self.version = ''
