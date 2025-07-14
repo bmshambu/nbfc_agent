e8980fe4572908cbade0efe3b3e43d4f05eb5feb
Shambu <46180328+bmshambu@users.noreply.github.com>
2025-07-15 00:48:50 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 class AppConfig:
     def __init__(self):
         self.api_key_groq="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"
-        # self.groq_model_name="llama-3.3-70b-versatile"
+        self.groq_model_name="llama-3.3-70b-versatile"
         self.api_key = "AIzaSyC5XasXZlcarmvDHodbV5R2TPPURfzxANU"
         self.base_url = ''
         self.version = ''
